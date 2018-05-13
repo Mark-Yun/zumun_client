77523c251db7f5457a41563143d4a8130b2d2f23
MarkYun <mark.yun89@gmail.com>
2018-05-13 19:29:44 +0900

    new impl store server debug utils

---
+++ storeServer/src/debug/java/com/mark/zumo/server/store/p2p/DebugUtil.java
@@ -0,0 +1,26 @@
+package com.mark.zumo.server.store.p2p;
+
+import android.os.Build;
+
+import com.mark.zumo.client.core.entity.MenuOrder;
+import com.mark.zumo.client.core.entity.Store;
+import com.mark.zumo.client.core.entity.user.GuestUser;
+
+/**
+ * Created by mark on 18. 5. 3.
+ */
+
+class DebugUtil {
+
+    static GuestUser testCustomerUser() {
+        return new GuestUser(0, Build.MODEL, 0);
+    }
+
+    static Store testStore() {
+        return new Store(5, Build.MODEL, 0, 0, 31, 31);
+    }
+
+    static MenuOrder testOrder() {
+        return new MenuOrder(1, 2, 3, null, 5, 100);
+    }
+}
