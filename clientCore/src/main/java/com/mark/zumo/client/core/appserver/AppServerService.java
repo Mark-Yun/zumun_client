204715ae5f1d15f9317d27962306625d110d2a76
MarkYun <mark.yun89@gmail.com>
2018-05-13 22:49:12 +0900

    recreated retrofit module

--- clientCore/src/main/java/com/mark/zumo/client/core/appserver/AppServerService.java
+++ clientCore/src/main/java/com/mark/zumo/client/core/appserver/AppServerService.java
@@ -1,24 +1,15 @@
 package com.mark.zumo.client.core.appserver;
 
-import com.mark.zumo.client.core.entity.user.User;
-
 import io.reactivex.Single;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
+import retrofit2.http.POST;
 
 /**
  * Created by mark on 18. 4. 30.
  */
 
 public interface AppServerService {
-    Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("https://faca5l5t89.execute-api.ap-northeast-2.amazonaws.com/zumo_api")
-            .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-            .build();
+    String URL = "https://faca5l5t89.execute-api.ap-northeast-2.amazonaws.com/zumo_api";
 
     @GET("users/guest/create")
     Single<String> createGuestUser();
--- clientCore/src/main/java/com/mark/zumo/client/core/appserver/AppServerServiceProvider.java
+++ clientCore/src/main/java/com/mark/zumo/client/core/appserver/AppServerServiceProvider.java
@@ -1,5 +1,12 @@
 package com.mark.zumo.client.core.appserver;
 
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by mark on 18. 4. 30.
  */
@@ -10,6 +17,33 @@ public enum AppServerServiceProvider {
     public final AppServerService service;
 
     AppServerServiceProvider() {
-        service = AppServerService.retrofit.create(AppServerService.class);
+        service = new Retrofit.Builder()
+                .baseUrl(AppServerService.URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .client(okHttpClient())
+                .build()
+                .create(AppServerService.class);
+    }
+
+    private OkHttpClient okHttpClient() {
+        return new OkHttpClient.Builder()
+                .addInterceptor(interceptor())
+                .build();
+    }
+
+    private Interceptor interceptor() {
+        return chain -> {
+            Request original = chain.request();
+
+            //TODO change header
+            Request request = original.newBuilder()
+                    .header("User-Agent", "Your-App-Name")
+                    .header("Accept", "application/vnd.yourapi.v1.full+json")
+                    .method(original.method(), original.body())
+                    .build();
+
+            return chain.proceed(request);
+        };
     }
 }
