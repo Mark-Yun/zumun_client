e809f1a944ed0bfa9c87df780f5396d0eaa520c6
MarkYun <mark.yun89@gmail.com>
2018-06-01 21:43:07 +0900

    update copyright by Mark Soft 2018

--- customerClient/src/main/java/com/mark/zumo/client/customer/view/order/OrderFragment.java
+++ customerClient/src/main/java/com/mark/zumo/client/customer/view/order/OrderFragment.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2018. Mark Soft - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
 package com.mark.zumo.client.customer.view.order;
 
 import android.os.Bundle;
--- storeServer/src/main/java/com/mark/zumo/server/store/view/SplashActivity.java
+++ storeServer/src/main/java/com/mark/zumo/server/store/view/SplashActivity.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2018. Mark Soft - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
 package com.mark.zumo.server.store.view;
 
 import android.arch.lifecycle.ViewModelProviders;
