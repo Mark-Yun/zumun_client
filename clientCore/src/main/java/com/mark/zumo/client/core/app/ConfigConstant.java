70a4f8b3c478e06f79fd079e7ac6434216c6cf44
MarkYun <mark.yun89@gmail.com>
2019-05-18 10:12:58 +0900

    1. update prd server 2. version up to 1.1.19

--- clientCore/src/main/java/com/mark/zumo/client/core/app/ConfigConstant.java
+++ clientCore/src/main/java/com/mark/zumo/client/core/app/ConfigConstant.java
@@ -13,13 +13,13 @@ import okhttp3.logging.HttpLoggingInterceptor;
 final class ConfigConstant {
 
     static final String APP_SERVER_DEV_URL = "https://rkvauion1l.execute-api.ap-northeast-2.amazonaws.com/";
-    static final String APP_SERVER_PRD_URL = "https://w93s89kgdc.execute-api.ap-northeast-2.amazonaws.com/";
+    static final String APP_SERVER_PRD_URL = "https://2rdoht00xi.execute-api.ap-northeast-2.amazonaws.com/";
 
     static final String APP_SERVER_DEV_API_KEY = "T29BotsPOE7WKnq9G0jEqaDvWTk0eY1T60Pf9d8W";
     static final String APP_SERVER_PRD_API_KEY = "5psv9IS6TF8kW4BpcBH1L9iYJ0Pgnx5n8W0QU0Xn";
 
     static final String PAYMENT_SERVICE_DEV_URL = "https://a20xx0onkb.execute-api.ap-northeast-2.amazonaws.com/";
-    static final String PAYMENT_SERVICE_PRD_URL = "https://6trq5villl.execute-api.ap-northeast-2.amazonaws.com/";
+    static final String PAYMENT_SERVICE_PRD_URL = "https://sat8n10t6k.execute-api.ap-northeast-2.amazonaws.com/";
 
     static final String KAKAO_PAY_TEST_CID = "TC0ONETIME";
 
